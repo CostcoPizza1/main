3fa725681fafe83acc41932c1e21da55c4b46d90
JamesTheAxolotl <87155032+CostocoPizza@users.noreply.github.com>
2024-02-19 17:32:06 -0500

    bootstrap hopefully works

---
+++ js/splashText.js
@@ -0,0 +1,16 @@
+var splashTextsList = [
+  "Skibidi Toilet",
+  "Costco Best",
+  "UwU",
+  "OwO",
+  "100% Skidded",
+  "Best skid on the web",
+  "Int Bg Rity",
+  "Rocoo Taco",
+  "Do You Have A Snowflake?",
+  "But he made graduation",
+  "Vscode.dev",
+  "Téléfrançais",
+  "Info seem familiar, don't talk shit about Costco Pizza",
+  "Dark humor is like food, not everyone gets it.",
+];
