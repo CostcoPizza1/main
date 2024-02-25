f5dc8205e013ed3e7f959ca541a7a4764b006a19
JamesTheAxolotl <87155032+CostocoPizza@users.noreply.github.com>
2024-02-24 20:02:18 -0500

    fix theme attempt 1

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,10 @@
-var themeSetting = document.getElementById('theme-selector');
+var themeSetting = document.getElementById("theme-selector");
 
-themeSetting.addEventListener('change', () => {
-    console.log("Changed theme to " + themeSetting.value)
-    document.body.setAttribute('theme', themeSetting.value);
-    localStorage.setItem("theme", themeSetting.value);
-})
+themeSetting.addEventListener("change", () => {
+  console.log("Changed theme to " + themeSetting.value);
+  document.body.setAttribute("theme", themeSetting.value);
+  localStorage.setItem("theme", themeSetting.value);
+});
 
-if (localStorage.getItem("theme")) {
-    document.body.setAttribute("theme", localStorage.getItem("theme"))
-} else {
-    document.body.setAttribute("theme", "costco")
-}
+document.body.setAttribute("theme", localStorage.getItem("theme"));
+console.log("Theme: " + localStorage.getItem("theme"));
